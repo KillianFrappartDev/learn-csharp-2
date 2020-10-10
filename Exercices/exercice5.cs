aa2f24e8fc24b566b7594b869e8b2aa2604b2241
KillianFrappartDev <KillianFrappartDev@gmail.com>
2020-10-10 15:20:04 +0200

    Finished

--- Exercices/exercice5.cs
+++ Exercices/exercice5.cs
@@ -4,8 +4,36 @@ namespace Exercices
 {
     public class Exercice5
     {
+        //     public static void Answer()
+        //     {
+        //         Console.Write("Enter numbers separated by a coma (1, 3, 7, ...) ");
+        //         var inputStr = Console.ReadLine();
+
+        //         var max = 0;
+        //         foreach (char num in inputStr)
+        //         {
+        //             if (num == Convert.ToChar(" ") || num == Convert.ToChar(",")) continue;
+        //             else
+        //             {
+        //                 if (Convert.ToInt32(num.ToString()) > max) max = Convert.ToInt32(num.ToString());
+        //             }
+        //         }
+        //         Console.WriteLine(max);
+        //     }
+
         public static void Answer()
         {
+            Console.Write("Enter numbers separated by a coma (1, 3, 7, ...) ");
+            var inputStr = Console.ReadLine();
+            var numArr = inputStr.Split(", ", inputStr.Length);
+            var max = Convert.ToInt32(numArr[0]);
+
+            for (var i = 1; i < numArr.Length; i++)
+            {
+                if (Convert.ToInt32(numArr[i]) > max) max = Convert.ToInt32(numArr[i]);
+            }
+
+            Console.WriteLine(max);
 
         }
     }
--- Program.cs
+++ Program.cs
@@ -7,10 +7,11 @@ namespace learn_csharp_2
     {
         static void Main(string[] args)
         {
-            // Exercice1.Answer();
-            // Exercice2.Answer();
-            // Exercice3.Answer();
+            Exercice1.Answer();
+            Exercice2.Answer();
+            Exercice3.Answer();
             Exercice4.Answer();
+            Exercice5.Answer();
         }
     }
 }
